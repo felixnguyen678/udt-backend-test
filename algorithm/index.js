d02c50b24498ab099ef15bc318f6c1353d2c2cf8
nguyenquangphuc-vn <nguyenquangphuc.vn.hue@gmail.com>
2022-03-12 10:47:40 +0700

    Section 1 | design algorithm

---
+++ algorithm/index.js
@@ -0,0 +1,66 @@
+const neededContainer = 10;
+const listings = [
+    {
+        name:  "Container renter A ",
+        container: 5,
+        totalCost: 5,
+    },
+    {
+        name:  "Container renter B ",
+        container: 2,
+        totalCost: 10,
+    },
+    {
+        name:  "Container renter C ",
+        container: 2,
+        totalCost: 3,
+    },
+];
+
+function rentContainer(neededContainer, listings) {
+    let remainingAmount = neededContainer;
+    let rentResult = {
+        isEnough: true,
+        contracts: [],
+    };
+    const sortedListings =  listings.sort((firstListing, secondListing) => (firstListing.totalCost > secondListing.totalCost) ? 1 : -1);
+    for(let i = 0; i < sortedListings.length; i++ ){
+        const listing = sortedListings[i];
+        if(listing.container > remainingAmount) {
+            rentResult.contracts.push({
+                name: listing.name,
+                container: remainingAmount,
+                price: listing.totalCost
+            })
+            break;
+        } else {
+            rentResult.contracts.push({
+                name: listing.name,
+                container: listing.container,
+                price: listing.totalCost
+            })
+            if(i === sortedListings.length -1 ){
+                rentResult.isEnough = false;
+            } else {
+                remainingAmount -= listing.container
+                if(remainingAmount === 0 ) {
+                    break;
+                }
+            }
+        }
+
+    }
+    printOutputFromRentResult(rentResult);
+}
+
+function printOutputFromRentResult(rentResult){
+    rentResult.contracts.map(contract => {
+        console.log(`[Contact with] ${contract.name}, price: ${contract.price}`)
+    })
+    if(!rentResult.isEnough) {
+        console.log('Not enough containers')
+    }
+    //TODO: total cost ...
+}
+
+rentContainer(neededContainer, listings)
