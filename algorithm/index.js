24186cb3188fdba2062608c80ad2adb05b03a9ba
nguyenquangphuc-vn <nguyenquangphuc.vn.hue@gmail.com>
2022-03-15 17:56:18 +0700

    Section 1 | update algorithm output print

--- algorithm/index.js
+++ algorithm/index.js
@@ -16,16 +16,17 @@ const listings = [
         totalCost: 3,
     },
 ];
-
 function rentContainer(neededContainer, listings) {
     let remainingAmount = neededContainer;
     let rentResult = {
         isEnough: true,
         contracts: [],
+        totalCost: 0
     };
     const sortedListings =  listings.sort((firstListing, secondListing) => (firstListing.totalCost > secondListing.totalCost) ? 1 : -1);
     for(let i = 0; i < sortedListings.length; i++ ){
         const listing = sortedListings[i];
+        rentResult.totalCost += listing.totalCost
         if(listing.container > remainingAmount) {
             rentResult.contracts.push({
                 name: listing.name,
@@ -55,12 +56,12 @@ function rentContainer(neededContainer, listings) {
 
 function printOutputFromRentResult(rentResult){
     rentResult.contracts.map(contract => {
-        console.log(`[Contact with] ${contract.name}, price: ${contract.price}`)
+        console.log(`[Contact with] ${contract.name} ${contract.container} container, price: ${contract.price}`)
     })
     if(!rentResult.isEnough) {
         console.log('Not enough containers')
     }
-    //TODO: total cost ...
+    console.log(`[Summary] total cost ${rentResult.totalCost}`)
 }
 
 rentContainer(neededContainer, listings)
